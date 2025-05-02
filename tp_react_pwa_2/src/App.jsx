6954f24e48a95fc4a52e6ce88e42a842cc8c768d
Rocio-Ayelen-Fernandez <rayelen.fernandez@est.fi.uncoma.edu.ar>
2025-05-01 23:22:50 -0300

    feat: Add navigation redirect from root path to home route

--- tp_react_pwa_2/src/App.jsx
+++ tp_react_pwa_2/src/App.jsx
@@ -3,7 +3,7 @@ import Home from './pages/Home/Home'
 import Details from './pages/Details/Details'
 import Favorites from './pages/Favorites/Favorites'
 import { ROUTES } from './const/routes'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 
 import './App.css'
@@ -17,6 +17,7 @@ function App() {
     
       <Routes>
       
+      <Route path="/" element={<Navigate to={ROUTES.home} />} />
         <Route element={ <Home />} path={ROUTES.home} />
         <Route element={ <Details />} path={ROUTES.details} />
         <Route element={ <Favorites />} path={ROUTES.favorites} /> 
