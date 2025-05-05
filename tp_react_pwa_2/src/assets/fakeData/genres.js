4b64aa4f60fb50cb6cf702915e087e212f5b38d4
FlorenciaRusso9606 <russoflorencia96@gmail.com>
2025-05-05 16:14:14 -0300

    Modify styles to CardGenre  and modify genres

--- tp_react_pwa_2/src/assets/fakeData/genres.js
+++ tp_react_pwa_2/src/assets/fakeData/genres.js
@@ -1,12 +1,34 @@
+import rockImg from "../img/rock.png"
+import popImg from "../img/pop.png"
+import rnb from "../img/rnb.png"
+import funk from "../img/funk.png"
+import disco from "../img/disco.png"
+import electronica from "../img/electronica.png"
+import kPop from "../img/k-pop.png"
+import cumbia from "../img/cumbia.png"
+import latina from "../img/latina.png"
+// svg files
+import amplificador from "../img/amplificador.svg"
+import microfono from "../img/microfono.svg"
+import estereo from "../img/estereo.svg"
+import saxofon from "../img/saxofon.svg"
+import bolaDisco from "../img/bola-de-disco.svg"
+import consola from "../img/consola.svg"
+import kPopIcon from "../img/k-pop.svg"
+import teclado from "../img/teclado.svg"
+import guitarra from "../img/guitarra.svg"
+
+
+
 export const genres = [
-    { id: "rock", name: "Rock", image: "https://picsum.photos/seed/picsum/200/200" },
-    { id: "pop", name: "Pop", image: "https://picsum.photos/seed/picsum/200/200" },
-    { id: "r&b", name: "R&B", image: "https://picsum.photos/seed/picsum/200/200" },
-    { id: "funk", name: "Funk", image: "https://picsum.photos/seed/picsum/200/200" },
-    { id: "disco", name: "Disco", image: "https://picsum.photos/seed/picsum/200/200" },
-    { id: "electronica", name: "Electrónica", image: "https://picsum.photos/seed/picsum/200/200" },
-    { id: "k-pop", name: "K-Pop", image: "https://picsum.photos/seed/picsum/200/200" },
-    { id: "cumbia", name: "Cumbia", image: "https://picsum.photos/seed/picsum/200/200" },
-    { id: "latina", name: "Latina", image: "https://picsum.photos/seed/picsum/200/200" },
+    { id: "rock", name: "Rock", image: rockImg, icon: amplificador},
+    { id: "pop", name: "Pop", image: popImg, icon: microfono},
+    { id: "r&b", name: "R&B", image: rnb,  icon:estereo},
+    { id: "funk", name: "Funk", image: funk, icon: saxofon},
+    { id: "disco", name: "Disco", image: disco, icon: bolaDisco},
+    { id: "electronica", name: "Electrónica", image: electronica, icon: consola},
+    { id: "k-pop", name: "K-Pop", image: kPop, icon: kPopIcon},
+    { id: "cumbia", name: "Cumbia", image: cumbia,  icon:teclado},
+    { id: "latina", name: "Latina", image: latina, icon: guitarra},
   ];
   
--- tp_react_pwa_2/src/components/CardGenre/CardGenre.jsx
+++ tp_react_pwa_2/src/components/CardGenre/CardGenre.jsx
@@ -1,17 +1,23 @@
-const CardGenre = ({ name, image, id, onClick }) => {
+const CardGenre = ({ name, image, id, icon, onClick }) => {
   return (
     <div
       onClick={() => onClick(id)}
-      className="relative rounded-xl overflow-hidden cursor-pointer shadow-md w-32 group" 
+      className="mb-6 p-4 rounded-2xl bg-white/10 backdrop-blur-xl shadow-md hover:shadow-2xl hover:scale-[1.03] transition-transform duration-300 ease-in-out cursor-pointer w-full sm:w-48 text-white border border-white/10 hover:border-white/30 flex flex-row sm:flex-col items-center sm:items-stretch" 
     >
+      <div className="relative w-20 h-20 sm:w-full sm:h-48 flex-shrink-0">
       <img
         src={image} 
         alt={name}
-        className="w-full h-full object-cover transition duration-300 group-hover:brightness-110"
+        className="w-full h-full object-cover transition duration-300"
       />
-      <div className="absolute bottom-0 left-0 right-0 top-0 bg-black/50 text-white p-2 flex items-center justify-center transition duration-300 group-hover:bg-black/40 group-hover:scale-105">
-      <p className="text-lg font-semibold">{name}</p>
+       <div className="absolute inset-0 bg-black/40"></div>
+        <div className="absolute bottom-0 left-0 right-0 top-0  text-white p-2 flex flex-col items-center justify-center  group-hover:scale-105">
+            <img src={icon} alt="icon" className="w-14 mb-1"/>
+          <p className="text-lg font-semibold">{name}</p>
       </div>
+      </div>
+      
+      
     </div>
   );
 };
