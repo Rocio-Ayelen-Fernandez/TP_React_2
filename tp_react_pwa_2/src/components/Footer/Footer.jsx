e816d0e149a869c0eed614428f192f24504f0461
FlorenciaRusso9606 <russoflorencia96@gmail.com>
2025-05-21 19:51:31 -0300

    Add useTranslation to Footer component

--- tp_react_pwa_2/src/components/Footer/Footer.jsx
+++ tp_react_pwa_2/src/components/Footer/Footer.jsx
@@ -1,11 +1,14 @@
 import { FaGithub, FaSpotify } from "react-icons/fa";
+import { useTranslation } from "react-i18next";
+
 const Footer = () =>{
+    const { t } = useTranslation();
     return(
         <footer className="bg-gradient-to-t from-slate-950 to-indigo-950 text-gray-300 p-6 mt-3">
         <div className="max-w-6xl mx-auto flex flex-col md:flex-row sm:justify-start md:justify-between items-center space-y-4 md:space-y-0 gap-x-1 ">
           {/* Cuentas de Github */}
           <div className="flex flex-col text-sm space-y-2 items-start ">
-            <span className="font-semibold text-white ">Perfiles de Github</span>
+            <span className="font-semibold text-white ">{t("Github_Profiles")}</span>
             <a href="https://github.com/Rocio-Ayelen-Fernandez" target="_blank" className="flex items-center gap-x-1 transition-transform hover:scale-105 hover:brightness-125">
             <FaGithub />rocio-fernandez
             </a>
@@ -19,15 +22,15 @@ const Footer = () =>{
   
           {/* Links útiles */}
           <div className="flex flex-col text-sm space-y-2 items-start">
-            <span className="font-semibold text-white">Enlaces útiles</span>
+            <span className="font-semibold text-white">{t("Useful_Links")}s</span>
             <a href="https://github.com/Rocio-Ayelen-Fernandez/TP_React_2" target="_blank" className="flex items-center gap-x-1 transition-transform hover:scale-105 hover:brightness-125">
-            <FaGithub />Repositorio del Proyecto
+            <FaGithub />{t("Proyect_Repository")}
             </a>
             <a href="https://developer.spotify.com/documentation/web-api" target="_blank" className="flex items-center gap-x-1 transition-transform hover:scale-105 hover:brightness-125">
-            <FaSpotify /> Documentación de Spotify
+            <FaSpotify />{t("Spotify_Documentation")}
             </a>
             <a href="https://spotify.com" target="_blank"  className="flex items-center gap-x-1 transition-transform hover:scale-105 hover:brightness-125">
-            <FaSpotify /> Página oficial de Spotify
+            <FaSpotify />{t("Oficial_Spotify_Page")}
             </a>
           </div>
         </div>
